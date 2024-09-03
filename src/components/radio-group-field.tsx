6cb85d985b29fc0e2ab4c8d48065437e31a149f1
Paul Ochieng <paulokello273@gmail.com>
2024-09-03 14:11:17 +0300

    chore: Add radio group field component

---
+++ src/components/radio-group-field.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import {
+  FormField,
+  FormItem,
+  FormLabel,
+  FormControl,
+  FormMessage,
+} from "./ui/form";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { Label } from "./ui/label";
+import { UseFormReturn } from "react-hook-form";
+
+interface RadioOption {
+  value: string;
+  label: string;
+}
+
+interface RadioGroupFieldProps {
+  name: string;
+  question: string;
+  options: RadioOption[];
+  form: UseFormReturn<any>;
+}
+
+export const RadioGroupField: React.FC<RadioGroupFieldProps> = ({
+  name,
+  question,
+  options,
+  form,
+}) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className="space-y-3">
+          <FormLabel>{question}</FormLabel>
+          <FormControl>
+            <RadioGroup
+              onValueChange={field.onChange}
+              defaultValue={field.value}
+              className="grid grid-cols-1 md:grid-cols-2 gap-4"
+            >
+              {options.map((option) => (
+                <FormItem
+                  className="flex items-center space-x-1 space-y-0"
+                  key={option.value}
+                >
+                  <FormControl>
+                    <RadioGroupItem value={option.value} />
+                  </FormControl>
+                  <Label htmlFor={option.value}>{option.label}</Label>
+                </FormItem>
+              ))}
+            </RadioGroup>
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
