9326737a792648ad062328b3bbe9064ba778f5cf
Paul Ochieng <paulokello273@gmail.com>
2024-09-28 13:10:30 +0300

    chore: healthcare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,9 @@ import { Toaster } from "@/components/ui/sonner";
 import { cn } from "../lib/utils";
 
 export const metadata: Metadata = {
-  title: "Sales Improvement Agenda",
+  title: "Healthcare Improvement",
   description:
-    "A survey to allow you to be more productive and more creative in your sales role.",
+    "A survey to allow you to be more productive and more creative in healthcare.",
 };
 
 const manrope = Manrope({
